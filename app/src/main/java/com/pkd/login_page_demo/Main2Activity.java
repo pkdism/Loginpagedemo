0084fadcc3708c4a8c327b73620635d43e974a0f
pkdism <pawan.dwivedi94@gmail.com>
2019-12-08 14:49:12 +0530

    add login screen

--- app/src/main/java/com/pkd/login_page_demo/Main2Activity.java
+++ app/src/main/java/com/pkd/login_page_demo/Main2Activity.java
@@ -1,7 +1,15 @@
 package com.pkd.login_page_demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.lang.String;
+
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +17,23 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+//        Button submit_button = (Button)findViewById(R.id.id_submit);
+//        EditText email = (EditText)findViewById(R.id.id_email);
+//        EditText pw = (EditText)findViewById(R.id.id_pw);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView next_screen = (TextView)findViewById(R.id.id_login_succes);
+        next_screen.setText(message);
+
+//        String input_email = email.getText().toString();
+//        String input_pw = pw.getText().toString();
+//
+//        if (input_email.equals("john.doe@example.com") && input_pw.equals("12345678")) {
+//            next_screen.setText("Login Successful");
+//        } else {
+//            next_screen.setText("Incorrect Email or Password");
+//        }
     }
 }
--- app/src/main/java/com/pkd/login_page_demo/MainActivity.java
+++ app/src/main/java/com/pkd/login_page_demo/MainActivity.java
@@ -2,12 +2,50 @@ package com.pkd.login_page_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.text.Editable;
+import android.widget.TextView;
+
+import java.lang.String;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.pkd.login_page_demo.MESSAGE";
+//    private Button submit_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        submit_button = (Button) findViewById(R.id.id_submit);
+//        submit_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openActivity2();
+//            }
+//        });
     }
+
+
+    public void nextAct(View v) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        EditText email = (EditText)findViewById(R.id.id_email);
+        EditText pw = (EditText)findViewById(R.id.id_pw);
+        String input_email = email.getText().toString();
+        String input_pw = pw.getText().toString();
+        String message = "";
+        if (input_email.equals("john.doe@example.com") && input_pw.equals("12345678")) {
+            message = "Login Successful";
+        } else {
+            message = "Incorrect Email or Password";
+        }
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
 }
