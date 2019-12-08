3b18a9142ad23f07977d78e6df8a0ea4b666ffb0
pkdism <pawan.dwivedi94@gmail.com>
2019-12-08 18:51:00 +0530

    add espresso tests

--- app/src/androidTest/java/com/pkd/login_page_demo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/pkd/login_page_demo/ExampleInstrumentedTest.java
@@ -2,11 +2,23 @@ package com.pkd.login_page_demo;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
+import org.hamcrest.Matcher;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -14,13 +26,46 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+//public class TestMainActivity {
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    /**
+     * Perform action of waiting for a specific time.
+     */
+    public static ViewAction waitFor(final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return "Wait for " + millis + " milliseconds.";
+            }
+
+            @Override
+            public void perform(UiController uiController, final View view) {
+                uiController.loopMainThreadForAtLeast(millis);
+            }
+        };
+    }
+
     @Test
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.pkd.login_page_demo", appContext.getPackageName());
+        onView(withId(R.id.id_email)).perform(typeText("rashmi@dish.com"));
+        onView(withId(R.id.id_pw)).perform(typeText("12345678"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.id_submit)).perform(click());
+        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+//        assertEquals("com.pkd.login_page_demo", appContext.getPackageName());
+        onView(isRoot()).perform(waitFor(5000));
     }
 }
